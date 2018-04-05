fadd0f72d06df03ad151b0a537c46a85d32bf640
rovo98 <2402323937@qq.com>
2018-04-05 19:57:45 +0800

    udpate HelloWorldAction.java

--- struts2-learning/src/main/java/com/rovo98/struts/helloworld/action/HelloWorldAction.java
+++ struts2-learning/src/main/java/com/rovo98/struts/helloworld/action/HelloWorldAction.java
@@ -10,13 +10,31 @@ import com.rovo98.struts.helloworld.model.MessageStore;
  */
 public class HelloWorldAction extends ActionSupport {
     private MessageStore messageStore;
+    private static int helloCount = 0;
+    private String userName;
 
     public String execute() {
         messageStore = new MessageStore();
+        helloCount++;
+        if (userName != null) {
+            messageStore.setMessage(messageStore.getMessage() + " " + userName);
+        }
         return SUCCESS;
     }
 
+    // Getters and Setters.
     public MessageStore getMessageStore() {
         return messageStore;
+    }
+    public int getHelloCount() {
+        return helloCount;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
